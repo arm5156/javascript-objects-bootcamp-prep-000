7a5a8ecf4dbb8ed17fff0aa308375c582f0cd84c
Adam R Miller <arm5156@gmail.com>
2019-02-25 22:04:52 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,6 @@ function updatePlaylist (playlist,artistName,songTitle){
   return playlist
 }
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName
-  delete playlist.songTitle
+  
   return playlist
 }
